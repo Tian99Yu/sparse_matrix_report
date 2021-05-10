18da01d45a22901ded6cb1d6863981127c216c9d
Tian99Yu <yu_tian99@hotmail.com>
2021-05-10 16:33:12 +0800

    refactor serial

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #include <string.h>
 #include <time.h>
 
-#include "read.h"
 #include "mmio.h"
+#include "read.h"
 #include "time_util.h"
 /*
 * Lower triangular solver Lx=b
@@ -25,7 +25,8 @@ int lsolve(int n, int *Lp, int *Li, double *Lx, double *x)
     /* check inputs */
     for (j = 0; j < n; j++)
     {
-        if (Lx[Lp[j]] != 1){
+        if (Lx[Lp[j]] != 1)
+        {
             printf("this diagonal value is wrong, val: %f, col %d, row %d", Lx[Lp[j]], j, Lp[j]);
         }
         x[j] /= Lx[Lp[j]];
@@ -47,7 +48,10 @@ int lsolve_improve_1(int n, int *Lp, int *Li, double *Lx, double *x)
     for (j = 0; j < n; j++)
     {
         //check if x[j] is 0 to save time
-        if (x[j]==0){continue;}
+        if (x[j] == 0)
+        {
+            continue;
+        }
         x[j] /= Lx[Lp[j]];
         for (p = Lp[j] + 1; p < Lp[j + 1]; p++)
         {
@@ -56,7 +60,6 @@ int lsolve_improve_1(int n, int *Lp, int *Li, double *Lx, double *x)
     }
     return (1);
 }
-
 
 int lsolve_improve_2(int n, int *Lp, int *Li, double *Lx, double *x)
 {
@@ -68,17 +71,19 @@ int lsolve_improve_2(int n, int *Lp, int *Li, double *Lx, double *x)
     for (j = 0; j < n; j++)
     {
         //check if x[j] is 0 to save time
-        if (x[j]==0){continue;}
+        if (x[j] == 0)
+        {
+            continue;
+        }
         x[j] /= Lx[Lp[j]];
         x_tmp = x[j];
         for (p = Lp[j] + 1; p < Lp[j + 1]; p++)
         {
-          x[Li[p]] -= Lx[p] * x_tmp; }
+            x[Li[p]] -= Lx[p] * x_tmp;
+        }
     }
     return (1);
 }
-
-
 
 int lsolve_DFS_traversal(int n, int *Lp, int *Li, double *Lx, double *x)
 {
@@ -86,35 +91,41 @@ int lsolve_DFS_traversal(int n, int *Lp, int *Li, double *Lx, double *x)
     if (!Lp || !Li || !x)
         return (0);
     /* check inputs */
-    int * visited = malloc(n * sizeof(int));
+    int *visited = malloc(n * sizeof(int));
     memset(visited, 0, sizeof(int) * n);
-    int * stack = malloc(n * sizeof(int));
-    int * non_zeros = malloc(n * sizeof(int));
+    int *stack = malloc(n * sizeof(int));
+    int *non_zeros = malloc(n * sizeof(int));
     int stack_size, non_zeros_size;
     stack_size = 0;
-    non_zeros_size=0;
+    non_zeros_size = 0;
     //init the stack to be non-zero elements of b
-    for (int i=0; i<n; i++){
-        if(x[i]!=0){
+    for (int i = 0; i < n; i++)
+    {
+        if (x[i] != 0)
+        {
             stack[stack_size] = i;
             stack_size++;
         }
     }
     int cur_element, cur_col;
     //apply DFS on the matrix to find all adjacent nodes
-    while(stack_size != 0){
+    while (stack_size != 0)
+    {
         //pop the element
-        cur_element = stack[stack_size -1];
+        cur_element = stack[stack_size - 1];
         stack_size--;
-        if (visited[cur_element]==1) continue;
+        if (visited[cur_element] == 1)
+            continue;
         //push it into the X array
         non_zeros[non_zeros_size] = cur_element;
         non_zeros_size++;
         //mark it as visited
         visited[cur_element] = 1;
         //push all the node's non-visited children to the stack
-        for (int i=Lp[cur_element]+1; i<Lp[cur_element +1]; i++){
-            if (visited[Li[i]] == 0){                       
+        for (int i = Lp[cur_element] + 1; i < Lp[cur_element + 1]; i++)
+        {
+            if (visited[Li[i]] == 0)
+            {
                 stack[stack_size] = Li[i];
                 stack_size++;
             }
@@ -122,11 +133,11 @@ int lsolve_DFS_traversal(int n, int *Lp, int *Li, double *Lx, double *x)
     }
 
     heapSort(non_zeros, non_zeros_size);
-    for(int i=0; i<non_zeros_size; i++)
+    for (int i = 0; i < non_zeros_size; i++)
     {
-        j=non_zeros[i];
+        j = non_zeros[i];
         x[j] /= Lx[Lp[j]];
-        for (p = Lp[j] + 1; p<Lp[j+1] ; p++)
+        for (p = Lp[j] + 1; p < Lp[j + 1]; p++)
         {
             x[Li[p]] -= Lx[p] * x[j];
         }
@@ -138,7 +149,6 @@ int lsolve_DFS_traversal(int n, int *Lp, int *Li, double *Lx, double *x)
     return (1);
 }
 
-
 int lsolve_improve_omp(int n, int *Lp, int *Li, double *Lx, double *x)
 {
     int p, j;
@@ -148,11 +158,14 @@ int lsolve_improve_omp(int n, int *Lp, int *Li, double *Lx, double *x)
     for (j = 0; j < n; j++)
     {
         //check if x[j] is 0 to save time
-        if (x[j]==0){continue;}
+        if (x[j] == 0)
+        {
+            continue;
+        }
         x[j] /= Lx[Lp[j]];
-        
-        #pragma omp parallel private(p) num_threads(2)
-        #pragma omp for
+
+#pragma omp parallel private(p) num_threads(2)
+#pragma omp for
         for (p = Lp[j] + 1; p < Lp[j + 1]; p++)
         {
             x[Li[p]] -= Lx[p] * x[j];
@@ -161,43 +174,49 @@ int lsolve_improve_omp(int n, int *Lp, int *Li, double *Lx, double *x)
     return (1);
 }
 
-
-
-int verification(Matrix * mtx, double* b, double* answer){
+int verification(Matrix *mtx, double *b, double *answer)
+{
     int nz, dim;
     nz = mtx->nz;
     dim = mtx->dim;
     double result[dim];
     memset(result, 0, sizeof(double) * dim);
-    for (int i=0;i<dim; i++){
-        if (answer[i] > 100000) printf("blow value, %f", answer[i]);
+    for (int i = 0; i < dim; i++)
+    {
+        if (answer[i] > 100000)
+            printf("blow value, %f", answer[i]);
     }
-    for(int col=0;col<dim; col++){
-        for(int i=mtx->Lp[col]; i < mtx->Lp[col+1];i++){
+    for (int col = 0; col < dim; col++)
+    {
+        for (int i = mtx->Lp[col]; i < mtx->Lp[col + 1]; i++)
+        {
             int row;
             double val;
             row = mtx->Li[i];
             val = mtx->Lx[i];
 
-        if (answer[col] > 100000) printf("blow answer, %f", answer[col]);
-        if (val > 100000) printf("blow value, %f", val);
-        if (answer[col] * val > 100000) printf("blow multiplication %f",val * answer[col] );
+            if (answer[col] > 100000)
+                printf("blow answer, %f", answer[col]);
+            if (val > 100000)
+                printf("blow value, %f", val);
+            if (answer[col] * val > 100000)
+                printf("blow multiplication %f", val * answer[col]);
             result[row] += answer[col] * val;
         }
-    } 
-    for (int i=0; i<dim;i++){
-        if (abs(b[i] - result[i]) > 0.0001){
+    }
+    for (int i = 0; i < dim; i++)
+    {
+        if (abs(b[i] - result[i]) > 0.0001)
+        {
             printf("b: %f, result: %f, iteration %d\n", b[i], result[i], i);
             return 0;
         }
     }
     return 1;
-
 }
 
-
-
-int get_time(int (*solver_pt)(int, int*, int*, double*, double*), Matrix* m, double * solution, double* time, double * verification_b){
+int get_time(int (*solver_pt)(int, int *, int *, double *, double *), Matrix *m, double *solution, double *time, double *verification_b)
+{
     //read matrices
     //get dimension
     struct timespec time_start, time_finish;
@@ -206,52 +225,41 @@ int get_time(int (*solver_pt)(int, int*, int*, double*, double*), Matrix* m, dou
     clock_gettime(CLOCK_MONOTONIC, &time_finish);
 
     struct timespec time_diff = difftimespec(time_finish, time_start);
-    * time = timespec_to_msec(time_diff);
+    *time = timespec_to_msec(time_diff);
 
     int validate_result;
     validate_result = verification(m, verification_b, solution);
     return validate_result;
 }
 
-double get_speedup(double baseline, double cur_time){
+double get_speedup(double baseline, double cur_time)
+{
     return baseline / cur_time;
 }
 
+int main(int argc, char *argv[])
+{
+    char *mtx_dir = argv[1];
+    char *b_dir = argv[2];
 
-
-int main(){
-
-    // double *solution1, *solution2, *solution3,* verification_b; // Matrix * m1, *debug_m;
-    // double t1, t2, t3;
-    // m1 = read_matrix("matrices/debug_2/matrix.mtx");
-    // read_b("matrices/debug_2/b.mtx", &solution1);
-    // // lsolve_DFS_traversal(m1->dim, m1->Lp, m1->Li, m1->Lx, solution1);
-
-    // read_b("matrices/debug_2/b.mtx", &verification_b);
-    // int r3 = get_time(&lsolve_DFS_traversal, m1, solution1, &t3, verification_b);
-    // for (int i=0; i<9; i++){
-    //     printf("%f\n", solution1[i]);
-
-    // }
-    // printf("time %f, verification %d\n", t3, r3);
-
-
-    //==================================================
-    double *solution1, *solution2, *solution3,* verification_b, s1, s2, s3;
-    Matrix * m1, *debug_m;
+    double *solution1, *solution2, *solution3, *verification_b, s1, s2, s3;
+    double su1, su2, su3;
+    Matrix *m1, *debug_m;
     double t1, t2, t3;
-    m1 = read_matrix("matrices/TSOPF_RS_b678_c2/TSOPF_RS_b678_c2.mtx");
-    read_b("matrices/TSOPF_RS_b678_c2/b_for_TSOPF_RS_b678_c2_b.mtx", &solution1);
-    read_b("matrices/TSOPF_RS_b678_c2/b_for_TSOPF_RS_b678_c2_b.mtx", &solution2);
-    read_b("matrices/TSOPF_RS_b678_c2/b_for_TSOPF_RS_b678_c2_b.mtx", &solution3);
-    read_b("matrices/TSOPF_RS_b678_c2/b_for_TSOPF_RS_b678_c2_b.mtx", &verification_b);
+    m1 = read_matrix(mtx_dir);
+    read_b(b_dir, &solution1);
+    read_b(b_dir, &solution2);
+    read_b(b_dir, &solution3);
+    read_b(b_dir, &verification_b);
 
     int r1 = get_time(&lsolve, m1, solution1, &t1, verification_b);
     int r2 = get_time(&lsolve_improve_1, m1, solution2, &t2, verification_b);
     int r3 = get_time(&lsolve_DFS_traversal, m1, solution3, &t3, verification_b);
-    printf("time %f, verification %d\n", t1, r1);
-    printf("time %f, verification %d\n", t2, r2);
-    printf("time %f, verification %d\n", t3, r3);
+    su1 = get_speedup(t1, t1);
+    su2 = get_speedup(t1, t2);
+    su3 = get_speedup(t1, t3);
+    printf("time %f, speed up %f, verification %d\n", t1, su1, r1);
+    printf("time %f, speed up %f, verification %d\n", t2, su2, r2);
+    printf("time %f, speed up %f, verification %d\n", t3, su3, r3);
     exit(0);
-
 }
---
+++ run_serial.sh
@@ -0,0 +1,2 @@
+make 
+./main.out matrices/TSOPF_RS_b678_c2/TSOPF_RS_b678_c2.mtx matrices/TSOPF_RS_b678_c2/b_for_TSOPF_RS_b678_c2_b.mtx
