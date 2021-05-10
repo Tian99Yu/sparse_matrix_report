3cda51c40b96e0b6afb6ce2e5c26ec7e80ae077e
Tian99Yu <yu_tian99@hotmail.com>
2021-05-11 01:59:33 +0800

    update command

--- run_serial.sh
+++ run_serial.sh
@@ -1,2 +1,2 @@
-make 
-valgrind --tool=callgrind --cache-sim=yes ./main.out matrices/torso1/torso1.mtx matrices/torso1/b_for_torso1.mtx 
+make
+./main.out matrices/torso1/torso1.mtx matrices/torso1/b_for_torso1.mtx 
