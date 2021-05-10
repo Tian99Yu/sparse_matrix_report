8f52e9f55117d15172f6b0945f8e03ece0f8f6eb
Tian99Yu <yu_tian99@hotmail.com>
2021-05-11 02:01:04 +0800

    update command

--- run_serial.sh
+++ run_serial.sh
@@ -1,2 +1,2 @@
 make
-./main.out matrices/torso1/torso1.mtx matrices/torso1/b_for_torso1.mtx 
+./main.out matrices/TSOPF_RS_b678_c2/TSOPF_RS_b678_c2.mtx matrices/TSOPF_RS_b678_c2/b_for_TSOPF_RS_b678_c2_b.mtx
