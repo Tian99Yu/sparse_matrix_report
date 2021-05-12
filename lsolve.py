9f29d7797a16cfa86fa4655bf1b7914d44a58e91
tian <yu_tian99@hotmail.com>
2021-05-13 01:16:09 +0800

    add py

---
+++ lsolve.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+def parse_row(r):
+    r = r.split(" ")
+    return [int(r[0]), int(r[1]), float(r[2])] 
+
+
+def read_mtx(dir):
+    with open(dir, "r") as f:
+        line = f.readline()
+        while line[0] == "%":
+            line=f.readline()
+        header = line.split(" ")
+        header = [int(i) for i in header]
+        nr, nc, nz = header[0], header[1], header[2]
+        #init the matrix
+        mtx = np.zeros((nr, nc))
+        for i in range(nz):
+            line = f.readline()
+            i, j, v = parse_row(line)
+            if(i >= j):
+                mtx[i, j] = v
+        for i in range(nr):
+            if mtx[i,i] == 0:
+                mtx[i,i] = 1
+        return mtx
+
+def read_b(dir):
+    with open(dir, "r") as f:
+        line = f.readline()
+        while line[0] == "%":
+            line = f.readline()
+        header = [int(i) for i in header]
+        header = line.split(" ")
+        nr, nc, nz = header[0], header[1], header[2]
+        b = np.zeros(nr)
+        for i in range(nz):
+            line = f.readline()
+            i, j, v = parse_row(line)
+            b[i] = v
+        return b
+
+
+L = read_mtx("matrices/torso1/torso1.mtx")
+b = read_b("matrices/torso1/b_for_torso1.mtx")
+print(np.linalg.solve(L, b))
+        
+
+
+    
